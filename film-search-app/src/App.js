b9f55fe4e3e1db3a3243b208128a48bc0bb1ae9b
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-07-23 23:20:58 +0100

    david needs the monitor, so I'm calling it a night.

--- film-search-app/src/App.js
+++ film-search-app/src/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Header from "./components/Header.js";
 import Search from "./components/Search.js";
 
+import Axios from "axios";
+
+const baseURL = "http://www.omdbapi.com/?apikey=6a79fe1d";
+
+
+// //API Import
+// Axios.get(`baseURL&t=${title}`).then(function(res) {
+//   console.log(res);
+// });
+
 class App extends Component {
+  const [state, setState] = useState({
+    search: "",
+    data: []
+  })
+
+  const handleInput = (event) => {
+    let search = event.target.value;
+    setState(prevState => {
+      return { ...prevState, search: search}
+    });
+
+    console.log(state.search);
+  }
   render() {
     return (
       <div>
         <Header />
-        <Search />
+        <Search handleInput={handleInput} />
       </div>
     );
   }
--- film-search-app/src/components/Search.js
+++ film-search-app/src/components/Search.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+//onClick handler
+
+//fx to join works with + if necessary
+
 class Search extends Component {
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="Film Name" id="title" />
+          <input type="text" placeholder="Film Name" name="title" />
           <input type="submit" value="Search" />
         </form>
       </div>
